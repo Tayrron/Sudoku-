387ab88321613963fc58ed62c83396c24725108f
Polina-VA <polinavalhaeva3@gmail.com>
2024-07-05 15:25:06 +0300

    feat: function to solve sudoku

--- sudoku.js
+++ sudoku.js
@@ -4,11 +4,43 @@ function read() {
    */
 }
 
-function solve() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции read.
-   * Возвращает игровое поле после попытки его решить.
-   */
+function solveSudoku(board) {
+  function isValid(board, row, col, num) {
+    for (let x = 0; x < 9; x++) {
+      if (
+        board[row][x] === num ||
+        board[x][col] === num ||
+        board[3 * Math.floor(row / 3) + Math.floor(x / 3)][
+          3 * Math.floor(col / 3) + (x % 3)
+        ] === num
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function solve(board) {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (board[row][col] === "-") {
+          for (let num = 1; num <= 9; num++) {
+            if (isValid(board, row, col, num.toString())) {
+              board[row][col] = num.toString();
+              if (solve(board)) {
+                return true;
+              } else {
+                board[row][col] = "-";
+              }
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  solve(board);
+  return board;
 }
 
 function isSolved() {
